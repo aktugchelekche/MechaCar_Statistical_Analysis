37fa89af8b5ed1ceeeecf486dcbdd6da312ae5f5
AktugChelekche <aktugcilekci@hotmail.com>
2022-05-14 18:02:45 -0500

    Summary Statistics :

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -5,3 +5,11 @@ head(mchcars) # displying the columns
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mchcars) #generate multiple linear regression model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mchcars) )
 
+
+suscoil<-read.csv("Suspension_Coil.csv", check.names = F, stringsAsFactors = F) # reading Suscoil.csv
+
+total_summary <-suscoil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance = var(PSI),SD = sd(PSI), .groups = 'keep' )
+total_summary # Creating Summary Statistics 
+
+lot_summary <- suscoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance = var(PSI),SD = sd(PSI), .groups = 'keep' )
+# Grouping By Lot 
